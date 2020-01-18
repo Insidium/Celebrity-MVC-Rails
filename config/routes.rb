babef2858fee67264533f60f17a0c31bc32ab46f
Ryan Challen <ryanchallen@outlook.com>
2020-01-18 16:24:06 +1000

    Minor comment edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "celebs#index"
   post "/create", to: "celebs#create"
   get "/show/:id", to: "celebs#show"

6321002404615a3c187fce8f5a210d3181081899
Ryan Challen <ryanchallen@outlook.com>
2020-01-18 16:19:25 +1000

    Completed CRUD methods and routes

--- app/controllers/celebs_controller.rb
+++ app/controllers/celebs_controller.rb
@@ -5,22 +5,34 @@ class CelebsController < ApplicationController
     
         #Show all celebrities
         def index
+            render json: @celebrities
         end
     
         #Show a single celebrity
         def show
+            if @celebrities[params[:id].to_i] == nil
+                render plain: "#{params[:id]} is not a known celebrity ID."
+            else
+                render json: @celebrities[params[:id].to_i]
+            end
         end
     
         #Create a new celebrity
         def create
+            @celebrities << {:name => params[:name], :notability => params[:notability]}
+            redirect_to "/"
         end
     
         #Update a celebrity
         def update
+            @celebrities[params[:id].to_i] = {:name => params[:name], :notability => params[:notability]}
+            redirect_to "/"
         end
     
         #Remove a celebrity
         def destroy
+            @celebrities.delete_at(params[:id].to_i)
+            redirect_to "/"
         end
     
         private
@@ -32,7 +44,6 @@ class CelebsController < ApplicationController
             ] unless session[:celebrities]
     
             @celebrities = session[:celebrities]
-            
+
         end
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "celebs#index"
+  post "/create", to: "celebs#create"
+  get "/show/:id", to: "celebs#show"
+  put "/update/:id", to: "celebs#update"
+  delete "/delete/:id", to: "celebs#destroy"
 end
